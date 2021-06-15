d8d1c5e0a9802fc1b075a810cc41faa83187e72c
icepy <xiangwenwe@foxmail.com>
2021-06-15 21:03:14 +0800

    chore: 重构 UI

--- src/pages/archived/index.js
+++ src/pages/archived/index.js
@@ -1,75 +1,100 @@
-import { Table } from 'antd';
+import { useState } from 'react';
+import ReactModal from 'react-modal';
 import archivedData from './archived.json';
-import { createUnique } from '../../shared'
+import { createUnique } from '../../shared';
 import './index.css';
 
 const list = archivedData.archived;
 
-const columns = [
-  {
-    title: 'Icon',
-    dataIndex: 'icon',
-    key: 'icon',
-    render: (icon) => {
-      return (
-        <img src={icon} className='archived-icon' alt=''/>
-      )
-    }
-  },
-  {
-    title: 'Symbol',
-    dataIndex: 'token',
-    key: 'token'
-  },
-  {
-    title: 'Rules',
-    dataIndex: 'rules',
-    key: 'rules',
-    render: (rules) => {
-      const { en_US } = rules;
-      return (
-        <div className='rules-container'>
-          {
-            en_US.map((r, i) => {
-              return (
-                <div key={createUnique()}>
-                  <span className='rules-number'>{i+1}. </span>{r}
-                </div>
-              )
-            })
-          }
-        </div>
-      )
-    }
-  },
-  {
-    title: 'Website',
-    dataIndex: 'url',
-    key: 'url',
-    render: (url) => {
-      return <a href={url} target='_blank' rel="noreferrer">{url}</a>
-    }
-  }
-]
-
-
+console.log('list',list)
 
 const Archived = (props) => {
-  // const { language } = props;
-  // console.log('language', language);
+  console.log('Archived props', props);
+  const { language } = props;
+  const [openModal, setOpenModal] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
+  console.log('openModal', openModal);
+  console.log('modalContent', modalContent);
+
+  const listSelector = (data) => {
+    setOpenModal(!openModal)
+    setModalContent(data);
+  }
+
+  const closeModal = () => {
+    setOpenModal(false);
+    setModalContent(null);
+  }
+
   return (
-    <div className='archived'>
-      <Table
-        columns={columns} 
-        dataSource={list}
-        bordered
-        pagination={{
-          defaultPageSize: 5
-        }}
-        rowKey={(record) => {
-          return record.name;
-        }}
-      />
+    <div className='archived-container'>
+      {
+        list.map((v) => {
+          return (
+            <div 
+              className='archived-box' 
+              key={createUnique()}
+              onClick={(e) => {
+                if (e.target.nodeName === 'A'){
+                  return false;
+                }
+                listSelector(v)
+              }}
+            >
+              <div className='archived-icon-container'> 
+                <img src={v.icon} alt='' className='archived-icon'/>
+              </div>
+              <div className='archived-iv'>
+                <div className='archived-symbol'>{v.name}</div>
+                <div className='archived-url overflow-ellipsis overflow-hidden'>
+                  <a 
+                    href={v.url}
+                    target='_blank'
+                    rel="noreferrer"
+                  >
+                    {v.url}
+                  </a>
+                </div>
+              </div>
+            </div>
+          )
+        })
+      }
+      {
+        modalContent ? (
+          <ReactModal
+            isOpen={openModal}
+            className='archived-modal'
+            overlayClassName='archived-overlay'
+            ariaHideApp={false}
+          >
+            <div className='archived-modal-content'>
+              <div className='archived-modal-header'>
+                <div className='archived-modal-header-icon'>
+                  <img src={modalContent.icon} alt='' />
+                </div>
+                <div className='archived-modal-header-symbol'>{modalContent.name}</div>
+              </div>
+              <div>
+                {
+                  modalContent.rules[language].map((v, i) => {
+                    return (
+                      <div className='archived-modal-rules' key={createUnique()}>
+                        <span className='rules-number'>{i+1}. </span>{v}
+                      </div>
+                    )
+                  })
+                }
+              </div>
+            </div>
+            <div className='archived-modal-buttons'>
+              <div className='archived-modal-button' onClick={closeModal}>
+                Cancel
+              </div>
+            </div>
+          </ReactModal>
+        ) : null
+      }
     </div>
   )
 }
