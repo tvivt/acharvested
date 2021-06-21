55b89eb02fccbc121ecbca0a919d8a6540b4f2c9
icepy <xiangwenwe@foxmail.com>
2021-06-21 21:46:34 +0800

    chore: update

--- src/components/Statement/Statement.js
+++ src/components/Statement/Statement.js
@@ -6,6 +6,13 @@ const Statement = (props) => {
   const { priceTotal } = props;
   return (
     <>
+      <div className='about-name'>
+        <p className='text-lg text-gray-900' style={{marginBottom: '5px'}}>介绍</p>
+        <div>
+          于 2021/06/12 宣布推出“桑田”，喻为春种秋收之意。在以太坊网络中，尤其是 DeFi NFT 的蓬勃发展，为早期加密用户以空投的形式发放了数百万美元。
+          桑田收集和存储了大量的空投数据，并对数据进行加工提炼和对比，研究当前与过去的空投规则，以及寻找一些有趣的项目。
+        </div>
+      </div>
       <div className='text-gray-500 text-xs'>
         <p className='text-lg text-gray-900' style={{marginBottom: '5px'}}>声明</p>
         <p className='statement-padding'>
@@ -17,6 +24,14 @@ const Statement = (props) => {
         <p className='statement-padding'>
           * 没有人知道未来的空投，但我们可以猜测并希望是对的
         </p>
+      </div>
+      <div className='statement-how-added text-gray-500 text-xs'>
+        <p className='text-lg text-gray-900' style={{marginBottom: '5px'}}>功能</p>
+        <p className='statement-padding'>* 归档历史空投让您阅读和了解</p>
+        <p className='statement-padding'>* 提供学习策略，用来提高捕获未来重要空投的概率</p>
+        <p className='statement-padding'>* 提供学习策略的操作指南，让您无须担心不知如何操作</p>
+        <p className='statement-padding'>* 提供潜在列表，猜测一些协议提前埋伏</p>
+        <p className='statement-padding'>* 提供 Web 通知，避免因为时效性而产生的损失</p>
       </div>
       <div className='statement-how-added text-gray-500 text-xs'>
         <p className='text-lg text-gray-900' style={{marginBottom: '5px'}}>价格</p>
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -12,6 +12,10 @@ const About = (props) => {
     <div className='about-container'>
       
       <div className='about-content text-gray-500 text-xs'>
+        <Statement 
+          accountTotal={accountTotal}
+          priceTotal={priceTotal}
+        />
         <div className='about-cooperation'>
           <p className='text-lg text-gray-900' style={{marginBottom: '5px'}}>社区</p>
           <div>即将到来</div>
@@ -54,25 +58,6 @@ const About = (props) => {
             </a>
           </div>
         </div>
-        <div className='about-name'>
-          <p className='text-lg text-gray-900' style={{marginBottom: '5px'}}>介绍</p>
-          <div>
-            于 2021/06/12 宣布推出“桑田”，喻为春种秋收之意。在以太坊网络中，尤其是 DeFi NFT 的蓬勃发展，为早期加密用户以空投的形式发放了数百万美元。
-            桑田收集和存储了大量的空投数据，对数据进行加工提炼，研究当前和过去的空投规则，以及寻找一些有趣的项目。
-          </div>
-        </div>
-        <div className='about-introduction'>
-          <p className='text-lg text-gray-900' style={{marginBottom: '5px'}}>功能</p>
-          <p className='introduction-padding'>* 归档历史空投让您阅读和了解</p>
-          <p className='introduction-padding'>* 提供学习策略，用来提高捕获未来重要空投的概率</p>
-          <p className='introduction-padding'>* 提供学习策略的操作指南，让您无须担心不知如何操作</p>
-          <p className='introduction-padding'>* 提供潜在列表，猜测一些协议提前埋伏</p>
-          <p className='introduction-padding'>* 提供 Web 通知，避免因为时效性而产生的损失</p>
-        </div>
-        <Statement 
-          accountTotal={accountTotal}
-          priceTotal={priceTotal}
-        />
       </div>
     </div>
   )
