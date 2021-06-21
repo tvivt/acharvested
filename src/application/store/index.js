71b14c080453f7cb77f975394cb25f9d29e1b061
icepy <xiangwenwe@foxmail.com>
2021-06-21 23:01:39 +0800

    chore: 暂存

---
+++ src/application/store/index.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+
+const store = configureStore({
+  reducer: {},
+  middleware: (getDefaultMiddleware) => {
+    return process.env.NODE_ENV === 'development'
+      ? getDefaultMiddleware().concat(logger)
+      : getDefaultMiddleware();
+  }
+});
+
+export default store;
